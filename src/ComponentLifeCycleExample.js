e1d1d815112e6d260ee1713812727266b3b2a665
JDNET\ay86452 <yadavangad@johndeere.com>
2019-04-01 01:35:09 -0500

    react-app

--- src/ComponentLifeCycleExample.js
+++ src/ComponentLifeCycleExample.js
@@ -1,5 +1,4 @@
-import React, {Component} from 'react';
-import ReactDom from 'react-dom'
+import React from 'react';
 
 class ComponentLifeCycleExample extends React.Component {
   constructor(props) {
--- src/index.js
+++ src/index.js
@@ -4,3 +4,6 @@ import './index.css';
 import ComponentLifeCycleExample from './ComponentLifeCycleExample';
 
 ReactDOM.render(<ComponentLifeCycleExample />, document.getElementById('root'));
+
+// setTimeout(() => {
+//   ReactDOM.unmountComponentAtNode(document.getElementById('root'));}, 10000);
